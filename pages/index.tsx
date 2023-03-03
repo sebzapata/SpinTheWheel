dc5062cadb4a67ff7d5156bb872ff244b367f615
Sebastian Zapata <sebastian.zapata@davies-group.com>
2023-03-03 16:44:39 +0000

    add logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,88 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Button, Container, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+import { WheelData } from "react-custom-roulette/dist/components/Wheel/types";
+const Wheel = dynamic(
+  () => import("react-custom-roulette").then((mod) => mod.Wheel),
+  { ssr: false }
+);
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [listOfFilms, setListOfFilms] = useState<string[]>([]);
+  const [currentFilm, setCurrentFilm] = useState("");
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const renderFilmAddSection = () => {
+    return (
+      <Box display="flex" marginBottom={3}>
+        <TextField
+          label="Film to watch"
+          variant="outlined"
+          sx={{ marginRight: "2rem" }}
+          value={currentFilm}
+          onChange={(e) => setCurrentFilm(e.target.value)}
+        />
+        <Button
+          variant="contained"
+          onClick={() => {
+            if (currentFilm) {
+              setListOfFilms((prevState) => [...prevState, currentFilm]);
+            }
+            setCurrentFilm("");
+          }}
+        >
+          Add to list
+        </Button>
+      </Box>
+    );
+  };
+
+  const renderWheelSpinnerSection = () => {
+    if (!listOfFilms.length) {
+      return (
+        <Typography textAlign="center">
+          The magic wheel will appear when a film is added to the list
+        </Typography>
+      );
+    }
+
+    const handleSpinClick = () => {
+      const newPrizeNumber = Math.floor(Math.random() * listOfFilms.length);
+      setPrizeNumber(newPrizeNumber);
+      setMustSpin(true);
+    };
+
+    const filmData: WheelData[] = listOfFilms.map((film, index) => ({
+      option: film,
+      style: { backgroundColor: index % 2 === 0 ? "green" : "yellow" },
+    }));
+
+    return (
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Wheel
+          mustStartSpinning={mustSpin}
+          prizeNumber={prizeNumber}
+          data={filmData}
+          onStopSpinning={() => {
+            setMustSpin(false);
+          }}
+        />
+        <Button onClick={handleSpinClick} variant="contained">
+          SPIN
+        </Button>
+      </Box>
+    );
+  };
+
   return (
     <>
       <Head>
@@ -14,110 +91,45 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "2rem",
+          minHeight: "100vh",
+        }}
+      >
+        <Box display="flex">
+          <Typography
+            variant="h1"
+            marginBottom={4}
+            textAlign="center"
+            sx={{
+              fontSize: {
+                xs: "1.5rem",
+                md: "1.75rem",
+                lg: "2rem",
+              },
+              lineHeight: { xs: "0.75rem", md: "1rem" },
+            }}
+          >
+            Seb & Almu's Fabulous&nbsp;
+            <Box
+              sx={{
+                display: {
+                  md: "none",
+                },
+              }}
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+              <br />
+            </Box>
+            Movie Picking Wheel!
+          </Typography>
+        </Box>
+        {renderFilmAddSection()}
+        {renderWheelSpinnerSection()}
+      </Container>
     </>
-  )
+  );
 }
